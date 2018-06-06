5148c3a193c53b91d6e4cc044fdfac0678904a9d
Ricardo D'O. Albanus <albanus@umich.edu>
2018-06-06 14:47:13 -0400

    Initial version of bmo.R

---
+++ src/bmo.R
@@ -0,0 +1,83 @@
+options(stringsAsFactors = F)
+
+suppressPackageStartupMessages(library(optparse))
+suppressPackageStartupMessages(library(fitdistrplus))
+suppressPackageStartupMessages(library(metap))
+suppressPackageStartupMessages(library(parallel))
+
+option_list = list(
+    make_option(c("--f1"), type="character", default=NULL, 
+                help="ATAC-seq NB file", metavar="character"),
+    make_option(c("--f2"), type="character", default=NULL, 
+                help="Co-occurring motifs file", metavar="character"),
+    make_option(c("-n", "--name"), type="character", default=NULL, 
+                help="Motif name", metavar="character"),
+    make_option(c("-o", "--output"), type="character", default=NULL, 
+                help="output directory", metavar="character"),
+    make_option(c("-p", "--parallel"), type="integer", default=1, 
+                help="Speed up things a bit with some parallelization", metavar="integer")
+)
+opt_parser <- OptionParser(option_list = option_list)
+args <- parse_args(opt_parser)
+
+# setwd("/lab/work/albanus/islet_BMO_redo")
+# args$f1 <- "negative_binomials/abe1388/ATF7_1.bed.gz"
+# args$f2 <- "raw_signal/abe1388/co-occurring/ATF7_1.bed"
+# args$output <- "test_ATF"
+# args$parallel <- 4
+
+# Print function
+sys_print <- function(x){write(x, stdout())}
+
+# Read data
+sys_print("Reading data...")
+df_nb     <- read.table(args$f1)
+df_motifs <- read.table(args$f2)
+
+# Fit NB distribution on co-occurring motifs
+sys_print("Fitting distributions...")
+co_occurring_motifs <- df_motifs$V8 - 1 # Make it zero-based for fitting NB
+fitted <- fitdist(co_occurring_motifs, 'nbinom', discrete = T, method = 'mle')
+parameters_fitted <- coef(fitted)
+
+# Get p-values
+pvals <- pnbinom(co_occurring_motifs, size = parameters_fitted['size'], 
+                 mu = parameters_fitted['mu'], lower.tail = F)
+
+# Make main data table
+bmo_df <- data.frame(df_nb[,1:6], 
+                     atac_score  = df_motifs$V7,
+                     motif_score = df_motifs$V8,
+                     tag_pval    = 10^(df_nb$V9 * -1),
+                     motif_pval  = pvals)
+
+# Combine p-values with sumz (Liptak's method)
+sys_print(paste("Calculating p-values with", args$parallel, "core(s)..."))
+combine_p <- function(x){
+    x <- as.numeric(x)
+    combined_pval <- sumz(x)$p
+    return(combined_pval)
+}
+
+cl <- makeCluster(args$parallel, type = 'FORK')
+bmo_df$combined_p <- parApply(cl, bmo_df[,c("tag_pval", "motif_pval")], 1, combine_p)
+stopCluster(cl)
+
+# Adjust p-values
+bmo_df$adj_pval <- p.adjust(bmo_df$combined_p, method = 'BY')
+called_sig <- sum(bmo_df$adj_pval <= 0.05)
+
+# Make output bed files
+sys_print("Writing output...")
+bmo_df$score <- -log10(bmo_df$adj_pval)
+bmo_df$bound <- bmo_df$adj_pval <= 0.05
+
+cols_to_print <- c(paste0("V", 1:4), "score", "V6")
+bmo_bound <- bmo_df[bmo_df$bound, cols_to_print]
+
+outfile1 <- paste0(args$output, "/all/", args$name, ".all.bed")
+outfile2 <- paste0(args$output, "/bound/", args$name, ".bound.bed")
+write.table(bmo_all, outfile1, quote = F, col.names = F, row.names = F, sep = "\t")
+write.table(bmo_bound, outfile2, quote = F, col.names = F, row.names = F, sep = "\t")
+
+sys_print("All done!")
