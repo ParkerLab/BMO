7f30e894925b30315cd6a1c052a284a46ee0f204
Ricardo D'O. Albanus <albanus@umich.edu>
2018-11-05 21:59:53 -0500

    Add conda environment and R dependencies

---
+++ conda/install_R_dependencies.R
@@ -0,0 +1,3 @@
+install.packages("optparse", repos = "https://cran.rstudio.com")
+install.packages("fitdistrplus", repos = "https://cran.rstudio.com")
+install.packages("metap", repos = "https://cran.rstudio.com")
